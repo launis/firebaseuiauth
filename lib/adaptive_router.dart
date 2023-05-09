94e14331f51ef6d31107bb7b49f764d3a20de121
launis <62068629+launis@users.noreply.github.com>
2023-05-09 23:37:46 +0300

    auth

--- lib/adaptive_router.dart
+++ lib/adaptive_router.dart
@@ -1,32 +1,28 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 
-// import 'go_router_refresh_stream.dart';
-
-final GlobalKey<NavigatorState> _rootNavigatorKey =
-    GlobalKey<NavigatorState>(debugLabel: 'root');
-final GlobalKey<NavigatorState> _shellNavigatorKey =
-    GlobalKey<NavigatorState>(debugLabel: 'shell');
-
 final GoRouter router = GoRouter(
   initialLocation: '/signIn',
-  navigatorKey: _rootNavigatorKey,
   debugLogDiagnostics: true,
-//      refreshListenable:
-//          GoRouterRefreshStream(authRepository.authStateChanges()),
   redirect: (context, state) {
     final User? currentUser = FirebaseAuth.instance.currentUser;
 
+    print('state.matchedLocation${state.matchedLocation}');
+    print('state.location${state.location}');
+    print('currentUser.emailVerified${currentUser?.emailVerified}');
+    print('currentUser.email${currentUser?.email}');
+
     if (!state.matchedLocation.startsWith('/forgot-password')) {
       if (currentUser == null) {
+        print('/signIn');
         return '/signIn';
       }
 
       if (!currentUser.emailVerified && currentUser.email != null) {
+        print('/verify-email');
         return '/verify-email';
       }
     }
@@ -34,58 +30,52 @@ final GoRouter router = GoRouter(
   },
   routes: <RouteBase>[
     /// Application shell
-    ShellRoute(
-      navigatorKey: _shellNavigatorKey,
-      builder: (BuildContext context, GoRouterState state, Widget child) {
-        return ScaffoldWithNavBar(child: child);
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: '/verify-email',
-          pageBuilder: (context, state) => NoTransitionPage(
-            child: EmailVerificationScreen(
-              actions: [
-                EmailVerifiedAction(
-                  () => context.pushReplacement('/profile'),
-                ),
-                AuthCancelledAction(
-                  (context) {
-                    FirebaseUIAuth.signOut();
-                    context.go('/signIn');
-                  },
-                ),
-              ],
+    GoRoute(
+      path: '/verify-email',
+      pageBuilder: (context, state) => NoTransitionPage(
+        child: EmailVerificationScreen(
+          actions: [
+            EmailVerifiedAction(
+              () => context.pushReplacement('/profile'),
             ),
-          ),
-        ),
-        GoRoute(
-          path: '/forgot-password',
-          pageBuilder: (context, state) => const NoTransitionPage(
-            child: ForgotPasswordScreen(),
-          ),
-        ),
-        GoRoute(
-          path: '/signIn',
-          pageBuilder: (context, state) => const NoTransitionPage(
-            child: CustomSignInScreen(),
-          ),
-        ),
-        GoRoute(
-          path: '/profile',
-          pageBuilder: (context, state) => NoTransitionPage(
-            child: ProfileScreen(
-              providers: [EmailAuthProvider()],
-              actions: [
-                SignedOutAction(
-                  (context) {
-                    context.pushReplacement('/signIn');
-                  },
-                ),
-              ],
+            AuthCancelledAction(
+              (context) async {
+                print('AuthCancelledAction');
+                await FirebaseUIAuth.signOut();
+                if (!context.mounted) return;
+                context.go('/signIn');
+              },
             ),
-          ),
+          ],
         ),
-      ],
+      ),
+    ),
+    GoRoute(
+      path: '/forgot-password',
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: ForgotPasswordScreen(),
+      ),
+    ),
+    GoRoute(
+      path: '/signIn',
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: CustomSignInScreen(),
+      ),
+    ),
+    GoRoute(
+      path: '/profile',
+      pageBuilder: (context, state) => NoTransitionPage(
+        child: ProfileScreen(
+          providers: [EmailAuthProvider()],
+          actions: [
+            SignedOutAction(
+              (context) {
+                context.pushReplacement('/signIn');
+              },
+            ),
+          ],
+        ),
+      ),
     ),
   ],
 );
@@ -119,59 +109,4 @@ class CustomSignInScreen extends ConsumerWidget {
           ],
         ),
       );
-}
-
-class ScaffoldWithNavBar extends StatelessWidget {
-  /// Constructs an [ScaffoldWithNavBar].
-  const ScaffoldWithNavBar({
-    required this.child,
-    Key? key,
-  }) : super(key: key);
-
-  /// The widget to display in the body of the Scaffold.
-  /// In this sample, it is a Navigator.
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return AdaptiveScaffold(
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.account_circle_outlined),
-          selectedIcon: Icon(Icons.account_circle),
-          label: 'Profile',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.login_outlined),
-          selectedIcon: Icon(Icons.login),
-          label: 'SignIn',
-        ),
-      ],
-      selectedIndex: _calculateSelectedIndex(context),
-      onSelectedIndexChange: (idx) => _onItemTapped(idx, context),
-      body: (_) => child,
-    );
-  }
-
-  static int _calculateSelectedIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).location;
-    if (location.startsWith('/profile')) {
-      return 0;
-    }
-    if (location.startsWith('/sigIn')) {
-      return 1;
-    }
-    return 0;
-  }
-
-  void _onItemTapped(int index, BuildContext context) {
-    switch (index) {
-      case 0:
-        GoRouter.of(context).push('/profile');
-        break;
-      case 1:
-        GoRouter.of(context).push('/signIn');
-        break;
-    }
-  }
 }
